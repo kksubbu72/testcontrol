77125eba110e25ffe8ee6b22a45957fd5e3662d6
Subramanian <subramanian@-C02F50UTMD6V.local>
2021-08-04 13:07:39 +0800

    deploy

--- manifests/site.pp
+++ manifests/site.pp
@@ -40,9 +40,6 @@ node 'cm201987.platform9.puppet.net.yaml' {
 
 node 'pm201987.platform9.puppet.net'{
   include puppet_metrics_dashboard::profile::master::install
-}
-
-node 'pm201987.platform9.puppet.net' {
   class { 'puppet_metrics_dashboard':
     add_dashboard_examples => true,
     overwrite_dashboards   => false,
